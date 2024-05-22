bda3cb17c187c426a257295d76ff7278d3b8c900
Leonel Guareschi <leitoguareschi@gmail.com>
2024-05-22 12:05:20 -0300

    clase 13 JS

--- ejemplo06.js
+++ ejemplo06.js
@@ -1,11 +1,11 @@
-const mes = parseInt(prompt('Ingrese mes del año';''))
+const mes = parseInt(prompt('Ingrese mes del año'; ''))
 
-if(mes == 1 || mes == 2 || mes == 3){
+if (mes == 1 || mes == 2 || mes == 3) {
     console.lof('corrsponde al primer trimestre del año')
-} else if (mes == 4 || mes == 5 || mes == 6){
+} else if (mes == 4 || mes == 5 || mes == 6) {
     console.log('corresponde al segundo trimestre del año')
-}else if (mes == 7 || mes == 8 || mes 9) {
+} else if (mes == 7 || mes == 8 || mes 9) {
     console.log('correponde al trecer trimestre del año')
-} else{
-    console.log('corresponde la cuarto trimestre del año')
+} else if (mes == 10 || mes == 11 || mes 12){
+console.log('corresponde la cuarto trimestre del año')
 }
