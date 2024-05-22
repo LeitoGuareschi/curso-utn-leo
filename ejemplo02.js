a9b3ecd29eb49e91360d974ac447b5dfbd9da078
Leonel Guareschi <leitoguareschi@gmail.com>
2024-05-22 12:03:22 -0300

    clase 13 JS

---
+++ ejemplo02.js
@@ -0,0 +1,5 @@
+const nombre = prompt('Ingrese su nombre', '');
+
+const edad = prompt('Ingrese su edad', '');
+
+console.log(`Mi nombre es ${nombre} y tengo ${edad}`);
---
+++ ejemplo06.js
@@ -0,0 +1,11 @@
+const mes = parseInt(prompt('Ingrese mes del año';''))
+
+if(mes == 1 || mes == 2 || mes == 3){
+    console.lof('corrsponde al primer trimestre del año')
+} else if (mes == 4 || mes == 5 || mes == 6){
+    console.log('corresponde al segundo trimestre del año')
+}else if (mes == 7 || mes == 8 || mes 9) {
+    console.log('correponde al trecer trimestre del año')
+} else{
+    console.log('corresponde la cuarto trimestre del año')
+}
