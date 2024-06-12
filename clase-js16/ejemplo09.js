1f4c975108bb4fe3a6607f279e60579ef5a026a1
Leonel Guareschi <leitoguareschi@gmail.com>
2024-06-11 22:21:08 -0300

    clase 16

---
+++ clase-js16/ejemplo09.js
@@ -0,0 +1,16 @@
+const numeros = [1,23,45,65,36,25];
+
+const dobles = numeros.map(function(num){
+    return num *2
+})
+
+const trilpe = numeros.map(function(num){
+    return num *3
+})
+
+
+console.log(numeros)
+
+console.log(dobles)
+
+console.log(trilpe)
