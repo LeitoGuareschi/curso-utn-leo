3fe34295e7e4e138f4dac398ed35b4c07815db39
Leonel Guareschi <leitoguareschi@gmail.com>
2024-06-28 19:16:19 -0300

    CLASE 17

--- ejemplo02.js
+++
@@ -1,9 +0,0 @@
-const numeros = [10, 9, 23, 34, 53, 78, 5, 6];
-
-const encontrado = numeros.find(num => num > 12);
-
-console.log(numeros)
-console.log(encontrado)
-
-
-// find > encuentra la primera coincidencia que cumpla con la condicion.
